fa31268fdd30b06b70b31f15cb16ec373ec3756c
Jake Goldman <jgoldman@caltech.edu>
2024-04-02 13:11:53 -0700

    fix binaries

--- Da_data/compile.sh
+++ Da_data/compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
 
-g++ run_2.cpp -laddm -o run_2
-g++ run_4condi.cpp -laddm -o run_4condi
-g++ run_05.cpp -laddm -o run_05
+g++ run_2.cpp -laddm -o run2
+g++ run_4condi.cpp -laddm -o run4condi
+g++ run_05.cpp -laddm -o run05
